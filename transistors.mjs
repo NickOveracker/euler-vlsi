8d2fdfe7be41c2c1d1e4337e99397d29cc91888a
Nick Overacker <nick.overacker@ymail.com>
2021-07-17 01:30:05 +0900

    First steps at Javascriptification

--- transistors.mjs
+++ transistors.mjs
@@ -2,17 +2,23 @@
 // 
 // (provide (all-defined-out))
 // (define ordem '(S . D))
-// 
+let order = ['S', 'D'];
+
 // (define Vdd  'Vdd)
 // (define Vss  'Vss)
 // (define Vout 'Out)
-// 
+let Vdd  = 'Vdd';
+let Vss  = 'Vss';
+let Vout = 'Out';
+
 // (define-struct transistor
 //   (id points) #:transparent)
-// 
+let transistor = (_id, _points) => { id: _id, points: _points };
+
 // (define-struct link
 //   (point position) #:transparent)
-// 
+let link = (_point, _position) => { point: _point, position: _position };
+
 // (define (points pud pdn)
 //   (let ((ids
 //          (map (lambda(i) (transistor-id i)) pud))
@@ -69,21 +75,36 @@
 // (define (filter-transistor id g)
 //   (car (filter-map
 //         (lambda(a) (and (equal? (transistor-id a) id) a)) g)))
-//   
+let filter_transistor = (id, g) => {
+  for(var ii = 0; ii < g.length; ii++) {
+    if(g[ii].transistor_id === id) return g[ii];
+  }
+
+  return null;
+}
+
+
 // (define (list-swap list1 n1 n2)
 //   (let* ((ele1 (list-ref list1 n1))
 //          (ele2 (list-ref list1 n2))
 //          (n-list (list-set list1 n1 ele2))
 //          (f-list (list-set n-list n2 ele1)))
 //     f-list))
-// 
+let list_swap = (list1, n1, n2) => {
+  let retList = [...list1];
+  retList[n1] = list1[n2];
+  retList[n2] = list1[n1];
+  return retList;
+};
+
 // (define (nodes pud pdn)
 //   (let [(nodes-pud (remove-duplicates
 //                     (flatten (map (lambda(a) (transistor-points a)) pud))))
 //         (nodes-pdn (remove-duplicates
 //                     (flatten (map (lambda(a) (transistor-points a)) pdn))))]
 //     (values nodes-pud nodes-pdn) ))
-// 
+
+
 // (define (index-of l x)
 //   (cond
 //     [(list? l)
@@ -91,24 +112,71 @@
 //     [(pair? l)
 //          (if (equal? (car l) x) 0
 //              1)]))
-// 
+let index_of = (l, x) => l.indexOf(x);
+
 // ;;pud e pdn tests - A(D + E) + BC
 // (provide pud pdn)
 // (define pud (list (make-transistor 'A '(P2 . Vdd)) (make-transistor 'D '(P1 . Vdd))
 //                   (make-transistor 'E '(P2 . P1)) (make-transistor 'B '(Out . P2)) (make-transistor 'C '(Out . P2))))
+export let pdn = () => [
+  make_transistor('A', 'P2',  'Vdd'),
+  make_transistor('D', 'P1',  'Vdd'),
+  make_transistor('E', 'P2',  'P1' ),
+  make_transistor('B', 'Out', 'P2' ),
+  make_transistor('C', 'Out', 'P2' )
+];
+
 // (define pdn (list (make-transistor 'A '(Out . P3)) (make-transistor 'D '(P3 . Vss))
 //                   (make-transistor 'E '(P3 . Vss)) (make-transistor 'B '(Out . P4)) (make-transistor 'C '(P4 . Vss))))
-// 
+export let pdn = () => [
+  make_transistor('A', 'Out', 'P3' ),
+  make_transistor('D', 'P3',  'Vss'),
+  make_transistor('E', 'P3',  'Vss'),
+  make_transistor('B', 'Out', 'P4' ),
+  make_transistor('C', 'P4',  'Vss')
+];
+
 // ;pud2 pdn2 - AB + E + CD
 // (provide pud2 pdn2)
 // (define pud2 (list (make-transistor 'A '(P1 . Vdd)) (make-transistor 'B '(P1 . Vdd))
 //                    (make-transistor 'E '(P2 . P1)) (make-transistor 'D '(Out . P2)) (make-transistor 'C '(Out . P2))))
+export let pud2 = () => [
+  make_transistor('A', 'P1',  'Vdd'),
+  make_transistor('B', 'P1',  'Vdd'),
+  make_transistor('E', 'P2',  'P1' ),
+  make_transistor('D', 'Out', 'P2' ),
+  make_transistor('C', 'Out', 'P2' )
+];
+
 // (define pdn2 (list (make-transistor 'A '(Out . P3)) (make-transistor 'B '(P3 . Vss))
 //                    (make-transistor 'E '(Out . Vss)) (make-transistor 'D '(Out . P4)) (make-transistor 'C '(P4 . Vss))))
+export let pdn2 = () => [
+  make_transistor('A', 'Out', 'P3' ),
+  make_transistor('B', 'P3',  'Vss'),
+  make_transistor('E', 'Out', 'Vss'),
+  make_transistor('D', 'Out', 'P4' ),
+  make_transistor('C', 'P4',  'Vss')
+];
+
 // 
 // ;;pud3 pdn3 - ABC + DE
 // (provide pud3 pdn3)
 // (define pud3 (list (make-transistor 'A '(P1 . Vdd)) (make-transistor 'B '(P1 . Vdd))
 //                    (make-transistor 'C '(P1 . Vdd)) (make-transistor 'D '(Out . P1)) (make-transistor 'E '(Out . P1))))
+export let pud3 = () => [
+  make_transistor('A', 'P1',  'Vdd'),
+  make_transistor('B', 'P1',  'Vdd'),
+  make_transistor('C', 'P1',  'Vdd'),
+  make_transistor('D', 'Out', 'P1' ),
+  make_transistor('E', 'Out', 'P1' )
+];
+
 // (define pdn3 (list (make-transistor 'A '(Out . P2)) (make-transistor 'B '(P2 . P3))
 //                    (make-transistor 'C '(P3 . Vss)) (make-transistor 'D '(P4 . Vss)) (make-transistor 'E '(Out . P4))))
+export let pdn3 = () => [
+  make_transistor('A', 'Out', 'P2' ),
+  make_transistor('B', 'P2',  'P3' ),
+  make_transistor('C', 'P3',  'Vss'),
+  make_transistor('D', 'P4',  'Vss'),
+  make_transistor('E', 'Out', 'P4' )
+];
